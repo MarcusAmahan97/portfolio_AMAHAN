e6b18d1d742b85657e164adb41f359c59ae5eb1d
MarcusAmahan97 <sdgcajoshamahan@gmail.com>
2025-11-28 15:48:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
+const themeButton = document.getElementById('theme-toggle');
+const wallpaperButton = document.getElementById('wallpaper-toggle');
+
+themeButton.addEventListener('click', () => {
+  body.classList.toggle('dark');
+});
+
+const wallpapers = [
+  "wallpaper1.jpg",
+  "wallpaper2.jpg",
+  "wallpaper3.jpg"
+];
+
+let currentWallpaperIndex = 0;
+
+body.style.backgroundImage = `url('${wallpapers[currentWallpaperIndex]}')`;
+body.style.backgroundSize = "cover";
+body.style.backgroundPosition = "center";
+
+wallpaperButton.addEventListener('click', () => {
+  currentWallpaperIndex = (currentWallpaperIndex + 1) % wallpapers.length;
+  body.style.backgroundImage = `url('${wallpapers[currentWallpaperIndex]}')`;
+  body.style.backgroundSize = "cover";
+  body.style.backgroundPosition = "center";
 });
